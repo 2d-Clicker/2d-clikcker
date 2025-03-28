9d5f8d4fe58b7a5515477ea18a9e40bbb459b907
sinjid7 <wldmstls01@naver.com>
2025-03-28 10:51:26 +0900

    [Add] 무기, 인벤토리 스크립트 추가

---
+++ Assets/2.Scripts/Weapon/Weapon.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public string weaponName; // 무기 이름  
+    public int damage; // 무기 데미지 
+    public int range; // 무기 범위 
+}
