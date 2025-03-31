74ca545da4ea58fdd29bf39c09ad1f46d41f77cc
dduwls <azs1719@naver.com>
2025-03-31 19:52:17 +0900

    [Feat] 이펙트 추가

---
+++ Assets/2.Scripts/KJH_EnemyStage/GenerateEnemyTest.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateEnemyTest : MonoBehaviour
+{
+    public RectTransform spawnParent; // Canvas 안에 적을 넣을 부모
+    public GameObject enemyUIPrefab; // ApplePrefab 등
+    public EnemyData enemyData;
+    public HandleAttack handleAttack;
+    public Image hpFill;
+
+    [HideInInspector] public Enemy currentEnemy;
+
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    public void SpawnEnemy()
+    {
+        if (currentEnemy != null)
+            Destroy(currentEnemy.gameObject);
+
+        GameObject enemyUI = Instantiate(enemyUIPrefab, spawnParent);
+        currentEnemy = enemyUI.GetComponent<Enemy>();
+        currentEnemy.Initialize(enemyData);
+        currentEnemy.hpFillImage = hpFill;
+        handleAttack.enemy = currentEnemy;
+    }
+}
